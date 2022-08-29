5df05fee3e145afbb946f413d830c130ab88ed18
Roger <rogelio.g.sandoval@gmail.com>
2022-08-29 16:49:45 -0500

    add more dependencies and firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'minna-c691d',
+    appId: '1:119319671134:web:ceca64513aefcae14d5fcf',
+    storageBucket: 'minna-c691d.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCINpcZfngVq8TuoRIfLiWwvXqAnRUoWWE',
+    authDomain: 'minna-c691d.firebaseapp.com',
+    messagingSenderId: '119319671134',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'minna-c691d',
+    appId: '1:119319671134:web:ceca64513aefcae14d5fcf',
+    storageBucket: 'minna-c691d.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCINpcZfngVq8TuoRIfLiWwvXqAnRUoWWE',
+    authDomain: 'minna-c691d.firebaseapp.com',
+    messagingSenderId: '119319671134',
+  },
   production: false
 };
 
