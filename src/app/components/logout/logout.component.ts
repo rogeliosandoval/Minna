986c376418a13cfab0bb8e8df971fdef650761f7
Roger <rogelio.g.sandoval@gmail.com>
2022-09-01 21:27:32 -0500

    create the logout component

---
+++ src/app/components/logout/logout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'logout',
+    templateUrl: './logout.component.html',
+    styleUrls: ['./logout.component.scss']
+})
+
+export class Logout {
+
+}
