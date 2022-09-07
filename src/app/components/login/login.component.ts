a653f8b01d56ec66006ccd1f04cf72c30bab6441
Roger <rogelio.g.sandoval@gmail.com>
2022-09-06 20:51:31 -0500

    update the login page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -30,8 +30,8 @@ export class Login {
     }
 
     ngOnInit(): void {
-        if (this.authService.userLoggedIn) {  // if the user's logged in, navigate them to the dashboard (NOTE: don't use afAuth.currentUser -- it's never null)
-            this.router.navigate(['/dashboard']);
+        if (this.authService.userLoggedIn) {  // if the user's logged in, navigate them home (NOTE: don't use afAuth.currentUser -- it's never null)
+            this.router.navigate(['/home']);
         }
     }
 
@@ -53,7 +53,7 @@ export class Login {
             this.isProgressVisible = false;  // no matter what, when the auth service returns, we hide the progress indicator
             if (result == null) {  // null is success, false means there was an error
                 console.log('logging in...');
-                this.router.navigate(['/dashboard']);  // when the user is logged in, navigate them to dashboard
+                this.router.navigate(['/home']);  // when the user is logged in, navigate them home
             }
             else if (result.isValid == false) {
                 console.log('login error', result);
