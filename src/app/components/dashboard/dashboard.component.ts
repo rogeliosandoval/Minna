e83f983b3c7f11c63a2fa9951c84a4149ee7c5f6
Roger <rogelio.g.sandoval@gmail.com>
2022-09-06 20:50:40 -0500

    add content to the dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,7 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable, Subscription } from 'rxjs';
+import { AuthService } from '../../services/auth.service'
+import { DatabaseService } from "src/app/services/database.service";
+import { IPost } from "src/app/models/IPost";
 
 @Component({
     selector: 'dashboard',
@@ -9,24 +12,41 @@ import { Observable } from 'rxjs';
     styleUrls: ['./dashboard.component.scss']
 })
 
-export class Dashboard {
+export class Dashboard implements OnInit, OnDestroy {
 
     user: Observable<any>;  // Example: store the user's info here (Cloud Firestore: collection is 'users', docId is the user's email, lower case)
+    username: any;
+    postData: IPost[] = [];
+    postSubscription!: Subscription;
 
-    constructor(public fireAuth: AngularFireAuth, public afs: AngularFirestore) {
+    constructor(public fireAuth: AngularFireAuth, public afs: AngularFirestore, private afAuth: AuthService, private dbservice: DatabaseService) {
         this.user = fireAuth.user;
     }
-
+    
     ngOnInit(): void {
         this.fireAuth.authState.subscribe(user => {
-            console.log('Dashboard: user', user);
-
+            // console.log('Dashboard: user', user);
+            
             if (user) {
                 let emailLower = user.email?.toLowerCase();
                 this.user = this.afs.collection('users').doc(emailLower).valueChanges();
+                this.username = user.displayName;
             }
+            
         });
+        
+        this.postSubscription = this.dbservice.getPosts().subscribe(data => {
+            this.postData = data;
+        })
+        
     }
 
+    ngOnDestroy(): void {
+        this.postSubscription && this.postSubscription.unsubscribe();
+    }
+
+    logout(): void {
+        this.afAuth.logoutUser();
+    }
 
 }
