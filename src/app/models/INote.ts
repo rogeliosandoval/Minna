4d93a6c7d8633a5bbf439815610e823a9a97ffd9
Roger <rogelio.g.sandoval@gmail.com>
2022-09-12 12:35:00 -0500

    create new interface for notes

---
+++ src/app/models/INote.ts
@@ -0,0 +1,10 @@
+export interface INote {
+    isEdited?: boolean;
+    id?: string;
+    name: string;
+    email: string;
+    date: string;
+    color: string;
+    title: string;
+    message: string;
+}
