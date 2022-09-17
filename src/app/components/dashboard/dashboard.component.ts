205388c91f0bdb4cf3723ed7d8e1bfed0a8558cd
Roger <rogelio.g.sandoval@gmail.com>
2022-09-17 13:01:00 -0500

    increase time on dashboard callback

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -68,7 +68,7 @@ export class Dashboard implements OnInit, OnDestroy {
                     } else {
                         this.postsExist = true;
                     }
-                }, 600);
+                }, 1000);
                 
             });
             
